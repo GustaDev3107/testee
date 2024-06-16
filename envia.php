6215a3669af9250d6b330a752f0fecda29a75889
GustaDev3107 <gustadev.3107@outlook.com>
2024-06-16 16:44:40 -0300

    edição de arquivos

--- envia.php
+++ envia.php
@@ -1,22 +1,26 @@
 <?php
 
-    $nome = addcslashes($_POST['nome']);
-    $email = addcslashes($_POST['email']);
-    $telefone = addcslashes($_POST['telefone']);
-    $mensagem = addcslashes($_POST['mensagem']);
-    
-    $para = "gustaDev.3107@outlook.com";
+// Coleta e sanitização dos dados do formulário
+$nome = htmlspecialchars($_POST['nome']);
+$email = htmlspecialchars($_POST['email']);
+$telefone = htmlspecialchars($_POST['telefone']);
+$mensagem = htmlspecialchars($_POST['mensagem']);
 
-    $assunto = 'Coleta de dados'
+// Configuração do destinatário e assunto do e-mail
+$para = "gustaDev.3107@outlook.com";
+$assunto = 'Coleta de dados';
 
-    $corpo ="Nome: ".$nome."\n"."E-mail:".$email."\n"."Telefone:".$telefone."\n"."Mensagem:".$mensagem;
+// Corpo do e-mail
+$corpo = "Nome: " . $nome . "\n" . "E-mail: " . $email . "\n" . "Telefone: " . $telefone . "\n" . "Mensagem: " . $mensagem;
 
-    $cabeca = "From: gustavo.silva031@outlook.com"."\n"."Reply-to: ".$email."\n"."X=Mailer:PHP/".phpversionsion();
+// Cabeçalhos do e-mail
+$cabeca = "From: gustavo.silva031@outlook.com" . "\n" . "Reply-to: " . $email . "\n" . "X-Mailer: PHP/" . phpversion();
 
-    if(mail($para,$assunto,$corpo,$cabeca)){
-        echo("Mensagem enviada com sucesso!")
-    }else{
-        echo("Houve um erro ao enviar a mensagem, verifique se \nos campos estão prenchido corretamente.")
-    }
+// Envio do e-mail
+if(mail($para, $assunto, $corpo, $cabeca)){
+    echo("Mensagem enviada com sucesso!");
+} else {
+    echo("Houve um erro ao enviar a mensagem, verifique se \nos campos estão preenchidos corretamente.");
+}
 
 ?>
